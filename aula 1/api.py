228051121c0c46f595228d53f5e3131455f3b3db
izaque <izaquesouzawbe@gmail.com>
2025-09-18 14:09:58 -0300

    Api basica em python

---
+++ aula 1/api.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+IDADE_ADULTA = 18
+
+@app.route('/verificar_idade', methods=['POST'])
+def verificar_idade():    
+    data = request.get_json()
+
+    nome = data.get('nome')
+    idade = data.get('idade')
+
+    idade_int = int(idade)
+
+    is_adult = (idade_int >= IDADE_ADULTA)
+
+    return jsonify({
+        "nome": str(nome).strip(),
+        "idade": idade_int,
+        "is_adult": is_adult,
+        "mensagem": "Maior de idade" if is_adult else "Menor de idade"
+    }), 200
+
+
+if __name__ == '__main__':
+    # roda localmente em http://127.0.0.1:5000
+    app.run(debug=True)
